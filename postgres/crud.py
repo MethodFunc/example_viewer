0734e8001a14a44e172e701cd4c246b72e38347a
Methodfunc <tjfgusfkd@gmail.com>
2022-11-23 08:27:18 +0900

    풍속 추가, 데이트포멧 처리

--- postgres/crud.py
+++ postgres/crud.py
@@ -8,16 +8,19 @@ from sqlalchemy.orm import Session
 
 from postgres import schemas, database, models
 
+DATEFORMAT_ = "%Y%m%d"
+DATEFORMAT = "%Y-%m-%d %H:%M:%S"
+
 
 def utc_convert(start_time: str, end_time: Optional[str] = None):
     pattern = r'[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]'
     start_time = re.sub(pattern, '', start_time)
-    start_time = datetime.strptime(start_time, "%Y%m%d")
-    start_time = (start_time - timedelta(hours=9)).strftime("%Y-%m-%d %H:%M:%S")
+    start_time = datetime.strptime(start_time, DATEFORMAT_)
+    start_time = (start_time - timedelta(hours=9)).strftime(DATEFORMAT)
     if end_time is not None:
         end_time = re.sub(pattern, '', end_time)
-        end_time = datetime.strptime(end_time, "%Y%m%d")
-        end_time = (end_time - timedelta(hours=9)).strftime("%Y-%m-%d %H:%M:%S")
+        end_time = datetime.strptime(end_time, DATEFORMAT_)
+        end_time = (end_time - timedelta(hours=9)).strftime(DATEFORMAT)
 
         return start_time, end_time
 
@@ -66,7 +69,8 @@ def hour_convert_forecast(db: Session, table_name: str, start_time: str, end_tim
     start_time, end_time = utc_convert(start_time, end_time)
     table = models.create_forcast(table_name)
     result = db.query(func.date_trunc('hour', func.timezone('KST', func.timezone('UTC', table.record_date))) \
-                      .label('ds'), func.avg(table.forecast).label('forecast')) \
+                      .label('ds'), func.avg(table.wind_speed).label('WS'),
+                      func.avg(table.active_power).label('actual')) \
         .order_by(text('1')) \
         .group_by(text('1')) \
         .filter(table.record_date >= start_time) \
